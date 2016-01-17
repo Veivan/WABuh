19374d6f8c4d8604b9b424a180f2d408260d2e0e
veivan <veivan@yandex.ru>
2016-01-18 00:23:07 +0300

    Token ported

--- src/base/ApiBase.java
+++ src/base/ApiBase.java
@@ -9,30 +9,31 @@ import settings.Constants;
 public class ApiBase {
 
 	public static String md5Custom(String st) {
-	    MessageDigest messageDigest = null;
-	    byte[] digest = new byte[0];
-	 
-	    try {
-	        messageDigest = MessageDigest.getInstance("MD5");
-	        messageDigest.reset();
-	        messageDigest.update(st.getBytes());
-	        digest = messageDigest.digest();
-	    } catch (NoSuchAlgorithmException e) {
-	        // тут можно обработать ошибку
-	        // возникает она если в передаваемый алгоритм в getInstance(,,,) не существует
-	        e.printStackTrace();
-	    }
-	 
-	    BigInteger bigInt = new BigInteger(1, digest);
-	    String md5Hex = bigInt.toString(16);
-	 
-	    while( md5Hex.length() < 32 ){
-	        md5Hex = "0" + md5Hex;
-	    }
-	 
-	    return md5Hex;
+		MessageDigest messageDigest = null;
+		byte[] digest = new byte[0];
+
+		try {
+			messageDigest = MessageDigest.getInstance("MD5");
+			messageDigest.reset();
+			messageDigest.update(st.getBytes());
+			digest = messageDigest.digest();
+		} catch (NoSuchAlgorithmException e) {
+			// тут можно обработать ошибку
+			// возникает она если в передаваемый алгоритм в getInstance(,,,) не
+			// существует
+			e.printStackTrace();
+		}
+
+		BigInteger bigInt = new BigInteger(1, digest);
+		String md5Hex = bigInt.toString(16);
+
+		while (md5Hex.length() < 32) {
+			md5Hex = "0" + md5Hex;
+		}
+
+		return md5Hex;
 	}
-	
+
 	/**
 	 * Process number/jid and turn it into a JID if necessary
 	 *
@@ -53,4 +54,21 @@ public class ApiBase {
 		return number;
 	}
 
+	public static String toSHA1(byte[] convertme) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return byteArrayToHexString(md.digest(convertme));
+	}
+
+	private static String byteArrayToHexString(byte[] b) {
+		String result = "";
+		for (int i = 0; i < b.length; i++) {
+			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+		}
+		return result;
+	}
 }
--- src/register/WaToken.java
+++ src/register/WaToken.java
@@ -7,7 +7,8 @@ import base.ApiBase;
 
 public class WaToken {
 
-	public static String GenerateRequestToken(String number, String platform) throws UnsupportedEncodingException {
+	public static String GenerateRequestToken(String number, String platform)
+			throws UnsupportedEncodingException {
 		String result = "";
 		switch (platform) {
 		case "Nokia":
@@ -17,39 +18,38 @@ public class WaToken {
 			break;
 
 		case "Android":
-		    String st = "/UIGKU1FVQa+ATM2A0za7G2KI9S/CwPYjgAbc67v7ep42eO/WeTLx1lb1cHwxpsEgF4+PmYpLd2YpGUdX/A2JQitsHzDwgcdBpUf7psX1BU=";
+			String st = "/UIGKU1FVQa+ATM2A0za7G2KI9S/CwPYjgAbc67v7ep42eO/WeTLx1lb1cHwxpsEgF4+PmYpLd2YpGUdX/A2JQitsHzDwgcdBpUf7psX1BU=";
 			String signature = "MIIDMjCCAvCgAwIBAgIETCU2pDALBgcqhkjOOAQDBQAwfDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFDASBgNVBAcTC1NhbnRhIENsYXJhMRYwFAYDVQQKEw1XaGF0c0FwcCBJbmMuMRQwEgYDVQQLEwtFbmdpbmVlcmluZzEUMBIGA1UEAxMLQnJpYW4gQWN0b24wHhcNMTAwNjI1MjMwNzE2WhcNNDQwMjE1MjMwNzE2WjB8MQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEUMBIGA1UEBxMLU2FudGEgQ2xhcmExFjAUBgNVBAoTDVdoYXRzQXBwIEluYy4xFDASBgNVBAsTC0VuZ2luZWVyaW5nMRQwEgYDVQQDEwtCcmlhbiBBY3RvbjCCAbgwggEsBgcqhkjOOAQBMIIBHwKBgQD9f1OBHXUSKVLfSpwu7OTn9hG3UjzvRADDHj+AtlEmaUVdQCJR+1k9jVj6v8X1ujD2y5tVbNeBO4AdNG/yZmC3a5lQpaSfn+gEexAiwk+7qdf+t8Yb+DtX58aophUPBPuD9tPFHsMCNVQTWhaRMvZ1864rYdcq7/IiAxmd0UgBxwIVAJdgUI8VIwvMspK5gqLrhAvwWBz1AoGBAPfhoIXWmz3ey7yrXDa4V7l5lK+7+jrqgvlXTAs9B4JnUVlXjrrUWU/mcQcQgYC0SRZxI+hMKBYTt88JMozIpuE8FnqLVHyNKOCjrh4rs6Z1kW6jfwv6ITVi8ftiegEkO8yk8b6oUZCJqIPf4VrlnwaSi2ZegHtVJWQBTDv+z0kqA4GFAAKBgQDRGYtLgWh7zyRtQainJfCpiaUbzjJuhMgo4fVWZIvXHaSHBU1t5w//S0lDK2hiqkj8KpMWGywVov9eZxZy37V26dEqr/c2m5qZ0E+ynSu7sqUD7kGx/zeIcGT0H+KAVgkGNQCo5Uc0koLRWYHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXdKtOrNTQcc0e+t";
 			String classesMd5 = "U+w9L64wtuyknyhiDVdM/w==";
 
-			byte[] key0 = Base64.getDecoder().decode(signature.getBytes("UTF-8"));
-			byte[] key1 = Base64.getDecoder().decode(classesMd5.getBytes("UTF-8"));
-			byte[] key2 = Base64.getDecoder().decode(st.getBytes("UTF-8"));
+			byte[] key0 = Base64.getDecoder().decode(signature.getBytes());
+			byte[] key1 = Base64.getDecoder().decode(classesMd5.getBytes());
+			byte[] key2 = Base64.getDecoder().decode(st.getBytes());
 
-			String data = new String(key0, "UTF-8") + new String(key1, "UTF-8") + number;
-			// str_repeat(char, 64)		
-			String opad = new String(new char[64]).replace("\0", ""+(char)(0x5C));
-			String ipad = new String(new char[64]).replace("\0", ""+(char)(0x36));
-			
-			/*
-			 * $key2 = base64_decode(
-			 * "/UIGKU1FVQa+ATM2A0za7G2KI9S/CwPYjgAbc67v7ep42eO/WeTLx1lb1cHwxpsEgF4+PmYpLd2YpGUdX/A2JQitsHzDwgcdBpUf7psX1BU="
-			 * ); 
-			 * $data = base64_decode($signature) . base64_decode($classesMd5)
-			 * . $phone;
-			 * 
-			 * $opad = str_repeat(chr(0x5C), 64); 
-			 * $ipad = str_repeat(chr(0x36), 64); 
-			 * for ($i = 0; $i < 64; $i++) { $opad[$i] = $opad[$i] ^
-			 * $key2[$i]; $ipad[$i] = $ipad[$i] ^ $key2[$i]; }
-			 * 
-			 * $output = hash("sha1", $opad . hash("sha1", $ipad . $data, true),
-			 * true);
-			 * 
-			 * return base64_encode($output);
-			 */
-			result = "qq";
+			String data = new String(key0) + new String(key1) + number;
+			// str_repeat(char, 64)
+			String opadst = new String(new char[64]).replace("\0", ""
+					+ (char) (0x5C));
+			String ipadst = new String(new char[64]).replace("\0", ""
+					+ (char) (0x36));
+
+			byte[] opad = opadst.getBytes();
+			byte[] ipad = ipadst.getBytes();
+
+			for (int i = 0; i < 128; i++) {
+				opad[i] = (byte) (opad[i] ^ key2[i]);
+				ipad[i] = (byte) (ipad[i] ^ key2[i]);
+			}
+
+			String opadxor = new String(opad);
+			String ipadxor = new String(ipad);
+
+			String hash1 = ApiBase.toSHA1((ipadxor + data).getBytes());
+			String hash2 = ApiBase.toSHA1((opadxor + hash1).getBytes());
+
+			result = Base64.getEncoder().encodeToString(hash2.getBytes());
 		}
-		
+
 		return result;
 	}
 
