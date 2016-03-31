1cc683fab89945593998ea2c1f2a253ed3695c48
veivan <veivan@yandex.ru>
2016-03-31 05:09:11 +0300

    Need 2 convrt UTF-8 char to WHAT?

--- chat/examples/_myTestGenerateKeys.php
+++ chat/examples/_myTestGenerateKeys.php
@@ -5,22 +5,28 @@ $password = '+01ILzqNnN36tm5+1OvxAc3WkoM=';
 //$password = 'ab';
 
 $enc  = base64_decode($password);
-
 	$return = "";
 	for($i = 0; $i < strlen($enc); $i++) {
 	        $return .= ' '.bin2hex(substr($enc, $i, 1));
     	}
 	echo "$return \n";
-
+echo "$enc \n";
 
 //$challengeData = array(0xf7, 0xe2, 0x86, 0xd2, 0x1c, 0xda, 0x51, 0x2c, 0xef, 0x9b, 0x65, 0xb8, 0xd2, 0x69, 0x20, 0xf9, 0x3c, 0x5d, 0x64, 0x87);
 $challengeData = array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 $challengeData = 'b';
 
-// file_put_contents(__DIR__.'/filename.txt', implode($challengeData));
+file_put_contents(__DIR__.'/filename.txt', $enc);
 
-      $keys = KeyStream::GenerateKeys($password, $challengeData);
+//      $keys = KeyStream::GenerateKeys($password, $challengeData);
 //      $keys = KeyStream::GenerateKeys(base64_decode($password), $challengeData);
+
+$strpass = substr($enc, 0, 1);
+//$strpass = "ыMH/:ЌњЭъ¶n~ФлсНЦ’ѓ"; 
+echo "$strpass \n";
+
+      $keys = KeyStream::GenerateKeys($strpass, $challengeData);
+
 	$return = "";
 	for($i = 0; $i < strlen($keys[0]); $i++) {
 	        $return .= ' '.bin2hex(substr($keys[0], $i, 1));
--- src/mtest/TestGenerateKeys.java
+++ src/mtest/TestGenerateKeys.java
@@ -1,12 +1,16 @@
 package mtest;
 
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 import chatapi.Funcs;
 import helper.KeyStream;
 
 public class TestGenerateKeys {
 	public static void RunTect() throws Exception {
-/*		char[] decodedpassword = { '�', 'M', 'H', '/', ':', '�', '�', '�', '�', '�',
-				'n', '~', '�', '�', '�', '', '�', '�', '�', '�' };
+/*		char[] decodedpassword = { 'ы', 'M', 'H', '/', ':', 'Ќ', 'њ', 'Э', 'ъ', '¶',
+				'n', '~', 'Ф', 'л', 'с', '', 'Н', 'Ц', '’', 'ѓ' };          
 		int[] challengeHex = {0xf7, 0xe2, 0x86, 0xd2, 0x1c, 0xda, 0x51, 0x2c, 0xef, 0x9b, 
 				0x65, 0xb8, 0xd2, 0x69, 0x20, 0xf9, 0x3c, 0x5d, 0x64, 0x87};
 		
@@ -20,6 +24,13 @@ public class TestGenerateKeys {
 		//char[] password = { 'a', 'b'};
 		//byte[] challengeData = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 		
+//		System.out.println(System.getProperty("sun.jnu.encoding"));
+//		System.out.println(System.getProperty("file.encoding"));
+		System.out.println(Charset.defaultCharset()); 
+
+//		System.setProperty("file.encoding", "UTF-8");
+//		System.out.println(Charset.defaultCharset()); 
+		
 		String pass = "+01ILzqNnN36tm5+1OvxAc3WkoM=";
 		//pass = "ab";
 		
@@ -28,11 +39,17 @@ public class TestGenerateKeys {
 
 		byte[] challengeData = "b".getBytes();
 		
-		String encpass = new String(TestFuncs.encryptPassword(pass) );
+		String encpass = new String(TestFuncs.encryptPassword(pass));  //StandardCharsets.UTF_8
 		
+		//fb 4d 48 2f 3a 8d 9c dd fa b6 6e 7e d4 eb f1 01 cd d6 92 83
+					
 //		char[] buffer = encpass.toCharArray();
-		char[] buffer = pass.toCharArray();
-		System.out.println(encpass);			
+//		char[] buffer = pass.toCharArray();
+//		char[] buffer = "ыMH/:ЌњЭъ¶n~ФлсНЦ’ѓ".toCharArray();
+		
+		byte[] zx = { (byte)-4};
+		char[] buffer = (new String(zx)).toCharArray();
+		System.out.println(buffer);			
 		
 		byte[][] keys = KeyStream.GenerateKeys(buffer, challengeData);
 		
